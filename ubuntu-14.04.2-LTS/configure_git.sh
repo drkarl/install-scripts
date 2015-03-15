d655b7c486ba04e3556c59037e3ec522e9fe0063
Carlos Manias <carlmani@ie.ibm.com>
2015-03-15 19:14:56 +0000

    Added script to configure git

---
+++ ubuntu-14.04.2-LTS/configure_git.sh
@@ -0,0 +1,6 @@
+#Basic git configuration
+
+# Set the environment variables or edit the screen before running
+
+git config --global user.name $GIT_USERNAME
+git config --global user.email $GIT_USER_EMAIL
