1c4e796fa6e43cd950616445eab495c9495ca231
drkarl <karlmaxxx@gmail.com>
2015-03-17 13:44:36 -0400

    Added cleanup script

--- ubuntu-14.04.2-LTS/user/00-run_all.sh
+++ ubuntu-14.04.2-LTS/user/00-run_all.sh
@@ -1,3 +1,4 @@
 ./01-configure_git.sh
 ./02-secure_ssh.sh
 ./03-user_configuration.sh
+./04-cleanup.sh
---
+++ ubuntu-14.04.2-LTS/user/04-cleanup.sh
@@ -0,0 +1,5 @@
+cd ~
+rm -rf apt-fast
+rm -rf install-scripts
+sudo rm -rf /root/apt-fast
+sudo rm -rf /root/install-scripts
