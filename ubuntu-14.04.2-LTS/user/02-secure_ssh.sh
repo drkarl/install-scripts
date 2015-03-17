3f14dde934e6af36d0e9e720609f55aec0b1a7e7
drkarl <karlmaxxx@gmail.com>
2015-03-17 13:28:22 -0400

    Checks for root

--- ubuntu-14.04.2-LTS/user/02-secure_ssh.sh
+++ ubuntu-14.04.2-LTS/user/02-secure_ssh.sh
@@ -1,6 +1,14 @@
 #Basic configuration to secure SSH
 
-echo -e "\033[33;31mWarning, if you continue the root user won't be able to login anymore!\033[0m"
+user=`whoami`
+
+if [ "$user" == "root" ]
+then
+  echo -e "\033[1;31mCan't run this script as root!\033[0m"
+  exit 1;
+fi
+
+echo -e "\033[1;31mWarning, if you continue the root user won't be able to login anymore!\033[0m"
 read -p "Continue (y/n)?" choice
 case "$choice" in
   y|Y ) echo "yes";;
@@ -15,11 +23,11 @@ sudo sed -i -e '/^PermitRootLogin/s/^.*$/PermitRootLogin no/' /etc/ssh/sshd_conf
 sudo sed -i -e '/^#PasswordAuthentication/s/^.*$/PasswordAuthentication no/' /etc/ssh/sshd_config
 
 # Allow login to the current user
-sudo sed -i -e '$aAllowUsers `whoami`' /etc/ssh/sshd_config  
+sudo sed -i -e '$aAllowUsers '"$user"'' /etc/ssh/sshd_config  
 
 # OPTIONAL - Change standard SSH port (22)
 # sudo sed -i -e '/^Port/s/^.*$/Port 2222/' /etc/ssh/sshd_config
 
 sudo restart ssh
-echo -e "\033[33;32mRoot can't login anymore, and only keys, no passwords, can be used!\033[0m"
-echo -e "\033[33;31mSSH with the user before logging out of this session to make sure you don't lock yourself out!\033[0m"
+echo -e "\033[1;32mRoot can't login anymore, and only keys, no passwords, can be used!\033[0m"
+echo -e "\033[1;31mSSH with the user before logging out of this session to make sure you don't lock yourself out!\033[0m"
