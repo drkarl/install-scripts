4205b846c0d97571ddd6cf28782e8f600e439316
drkarl <karlmaxxx@gmail.com>
2015-03-20 11:40:39 -0400

    Added jetty install script

---
+++ ubuntu-14.04.2-LTS/install/jetty/jetty.sh
@@ -0,0 +1,63 @@
+# create download directories
+mkdir -p ~/build/jetty
+cd ~/build/jetty
+ 
+# download
+version="9.2.10.v20150310"
+aria2c -o jetty-distribution-$version.tar.gz -s5 "http://eclipse.org/downloads/download.php?file=/jetty/stable-9/dist/jetty-distribution-$version.tar.gz&r=1"
+
+# extract the archive - creates directory jetty-distribution-....
+tar -xvf jetty-distribution-$version.tar.gz
+ 
+# rename jetty directory
+echo "AMF"
+sudo mv jetty-distribution-$version /opt/jetty
+echo "CHU"
+ 
+# create jetty user
+sudo useradd -U -s /bin/false jetty
+ 
+# Make the jetty directory be owned by the jetty user
+sudo chown -R jetty:jetty /opt/jetty
+ 
+#Create link for jetty.sh
+sudo ln -s /opt/jetty/bin/jetty.sh /etc/init.d/jetty
+
+# create /etc/default/jetty
+sudo sh -c ' printf " 
+JAVA_HOME=/usr/java/default # Path to Java
+JETTY_HOME=/opt/jetty  #Path to Jetty
+NO_START=0 # Start on boot
+JETTY_HOST=0.0.0.0 # Listen to all hosts
+JETTY_PORT=8080 # Run on this port
+JETTY_USER=jetty # Run as this user
+" > /etc/default/jetty'
+ 
+# make webapps writable
+sudo chmod o+w /opt/jetty/webapps
+
+#Cleanup
+rm -rf ~/build/jetty
+ 
+# check if the installation settings are ok
+/opt/jetty/bin/jetty.sh check
+ 
+# Start Jetty as service
+sudo service jetty start
+ 
+# Stop Jetty as service
+sudo service jetty stop
+ 
+# the server runs on the default port of 8080
+# http://localhost:8080/
+# To let Jetty automatically start on reboot execute
+sudo update-rc.d jetty defaults
+
+# deploy an app
+#cp myapp.war /opt/jetty/webapps
+ 
+#In case port conflicts, you can check wich application is blocking port 8080
+#sudo netstat -lnptu|grep ":8080"
+ 
+# monitor jetty log files
+#ls -l /opt/jetty/logs
