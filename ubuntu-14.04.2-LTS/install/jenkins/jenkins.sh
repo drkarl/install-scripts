1c96d7b38765815edf2719158430ab9977b9ee98
drkarl <karlmaxxx@gmail.com>
2015-03-21 16:55:14 -0400

    Added Jenkins installation script

---
+++ ubuntu-14.04.2-LTS/install/jenkins/jenkins.sh
@@ -0,0 +1,4 @@
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update -y
+sudo apt-fast install -y jenkins
