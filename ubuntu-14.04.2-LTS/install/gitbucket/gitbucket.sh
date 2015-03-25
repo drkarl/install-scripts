994bfe40893262b558a37eba1ef4f3015a264e8b
drkarl <karlmaxxx@gmail.com>
2015-03-25 15:13:01 -0400

    Fixed gitbucket script

--- ubuntu-14.04.2-LTS/install/gitbucket/gitbucket.sh
+++ ubuntu-14.04.2-LTS/install/gitbucket/gitbucket.sh
@@ -1,8 +1,8 @@
 #Requires Java
 #Requires an application server, Tomcat, Jetty, Glassfish, etc
-echo -e "\033[1;33m=                                     =\033[0m"
-echo -e "\033[1;33m=========[INSTALLING GitBucket]========\033[0m"
-echo -e "\033[1;33m=                                     =\033[0m"
+echo "\033[1;33m=                                     =\033[0m"
+echo "\033[1;33m=========[INSTALLING GitBucket]========\033[0m"
+echo "\033[1;33m=                                     =\033[0m"
 #webapps folder depends on the app container
 
 #Jetty
@@ -12,5 +12,5 @@ webapps=/opt/jetty/webapps
 #webapps=/opt/tomcat/webapps
 
 aria2c -s5 https://github.com/takezoe/gitbucket/releases/download/3.0/gitbucket.war
-sudo mv gitbucket $webapps
-echo -e "\033[1;32mGitbucket has been installed correctly\033[0m"
+sudo mv gitbucket.war $webapps
+echo "\033[1;32mGitbucket has been installed correctly\033[0m"
